ea4d7e21ad25bce0060cac5e9f61a006b90a1f74
tmjoris <tangajoris@gmail.com>
2025-06-20 15:59:52 +0300

    established connection, now using postgres as calc

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+use sqlx::Connection;
+use sqlx::Row;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let url = "postgres://pred-zero:jtcmb=bl=1234@localhost:5432/bookstore";
+    let mut conn = sqlx::postgres::PgConnection::connect(url).await?;
+
+    let res = sqlx::query("SELECT 1 + 1 as sum")
+        .fetch_one(&mut conn)
+        .await?;
+    
+    let sum: i32 = res.get("sum");
+    println!("1 + 1 = {}", sum);
+    Ok(())
 }
