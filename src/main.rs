4b9ba3c92446ef92f8391697aaa16ab441ac2219
tmjoris <tangajoris@gmail.com>
2025-06-20 16:02:22 +0300

    changes the standard connection to a connection pool

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,13 @@
 use std::error::Error;
-use sqlx::Connection;
 use sqlx::Row;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let url = "postgres://pred-zero:jtcmb=bl=1234@localhost:5432/bookstore";
-    let mut conn = sqlx::postgres::PgConnection::connect(url).await?;
+    let pool = sqlx::postgres::PgPool::connect(url).await?;
 
     let res = sqlx::query("SELECT 1 + 1 as sum")
-        .fetch_one(&mut conn)
+        .fetch_one(&pool)
         .await?;
     
     let sum: i32 = res.get("sum");
