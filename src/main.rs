561af0b1cd4f4b05fa1591efc84994b5126afebf
tmjoris <tangajoris@gmail.com>
2025-06-21 17:44:48 +0300

    added db migrations

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,8 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let url = "postgres://pred-zero:jtcmb=bl=1234@localhost:5432/bookstore";
     let pool = sqlx::postgres::PgPool::connect(url).await?;
 
+    sqlx::migrate!("./migrations").run(&pool).await?;
+
     let res = sqlx::query("SELECT 1 + 1 as sum")
         .fetch_one(&pool)
         .await?;
