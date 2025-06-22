8c10eaf713bc0d5ccd903792feb476f109b42633
tmjoris <tangajoris@gmail.com>
2025-06-23 00:31:49 +0300

    added read, write, and update methods

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,54 @@
 use std::error::Error;
 use sqlx::Row;
 
+#[derive(Debug)]
+struct Book {
+    pub title: String,
+    pub author: String,
+    pub isbn: String
+}
+
+async fn create(book: &Book, pool: &sqlx::PgPool) -> Result<(), Box<dyn Error>> {
+    let query = "INSERT INTO book (title, author, isbn) VALUES ($1, $2, $3)";
+
+    sqlx::query(query)
+        .bind(&book.title)
+        .bind(&book.author)
+        .bind(&book.isbn)
+        .execute(pool)
+        .await?;
+    
+    Ok(())
+}
+
+async fn update(book: &Book, isbn: &str, pool: &sqlx::PgPool
+) -> Result<(), Box<dyn Error>> {
+    let query = "UPDATE book SET title = $1, author = $2 WHERE isbn = $3";
+
+    sqlx::query(query)
+        .bind(&book.title)
+        .bind(&book.author)
+        .bind(isbn)
+        .execute(pool)
+        .await?;
+    
+    Ok(())
+}
+
+async fn read(conn: &sqlx::PgPool) -> Result<Book, Box<dyn Error>> {
+    let q = "SELECT title, author, isbn FROM book";
+    let query = sqlx::query(q);
+
+    let row = query.fetch_one(conn).await?;
+
+    let book = Book{
+        title: row.get("title"),
+        author: row.get("author"),
+        isbn: row.get("isbn")
+    };
+    Ok(book)
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let url = "postgres://pred-zero:jtcmb=bl=1234@localhost:5432/bookstore";
@@ -8,11 +56,19 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
     sqlx::migrate!("./migrations").run(&pool).await?;
 
-    let res = sqlx::query("SELECT 1 + 1 as sum")
-        .fetch_one(&pool)
-        .await?;
-    
-    let sum: i32 = res.get("sum");
-    println!("1 + 1 = {}", sum);
+    let book = Book{
+        title: "Salem's Lot".to_string(),
+        author: "Steven King".to_string(),
+        isbn: "978-0-385-00751".to_string(),
+    };
+
+    //create(&book, &pool).await?;
+      
+    //update(&book, &book.isbn, &pool).await?;
+
+    let result = read(&pool).await?;
+
+    println!("{:?}",result);
+
     Ok(())
 }
